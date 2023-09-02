0c449198b39179816a365c0fc692258a16de9133
ankitfutureprofilez <138786848+ankitfutureprofilez@users.noreply.github.com>
2023-09-02 10:23:06 +0530

    home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import About from "./About";
+import Find from "./Find";
 import Functionality from "./Functionality";
 
 export default function Home() {
   return <>
     <About />
     <Functionality />
+    <Find/>
   </>
 }
 
